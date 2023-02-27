3319f714dd853cbecb294e914a022916beaf83c8
Jonathan Alonso <JA1@users.noreply.github.com>
2023-02-27 15:08:58 -0500

    Update App.jsx

--- public/App.jsx
+++ public/App.jsx
@@ -1,20 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
+import * as React from 'react'
+import { createRoot } from 'react-dom/client';
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EyeChat</title>
-    <link href="./theme.css" rel="stylesheet">
-    <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
-    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
-    <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
-</head>
+class App extends React.Component {
+  constructor() {
+    super();
 
-<body>
-    <div id="root"></div>
-    <script src="out.js" type="module"></script>
-</body>
+    this.state = {
+      messages: [{
+        nick: 'username',
+        message: 'this is a test'
+      }]
+    }
+  }
 
-</html>
+  componentDidMount() {
+    this.socket = new WebSocket('wss://' + location.host);
+
+    this.socket.addEventListener('message', (event) => {
+      console.log(event);
+    });
+  }
+  handleInput(event) {
+    const target = event.target;
+
+    if (event.which == 13) {
+      console.log('enter key');
+
+      const messages = [...this.state.messages];
+      const newMessage = {
+        nick: 'sammich',
+        message: target.value
+      }
+
+      this.socket.send(target.value);
+
+    }
+    console.log(event);
+  }
+
+  render() {
+    return (
+      <>
+        <div id="message-container">{
+          this.state.messages.map(message => {
+            return <div className="message">
+              <div className="nick">{message.nick}: </div>
+              <div className="messageContent">{' ' + message.message}</div>
+            </div>
+          })
+        }</div>
+
+        <div className="input-container" onKeyUp={this.handleInput.bind(this)}>
+          <input placeholder="Type anything then press enter." />
+        </div>
+
+      </>
+    );
+  }
+
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
